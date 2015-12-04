148723626b63df3091ed1b4c51adf1529fa0712b
Alex Espinoza <aespinoza@aspria.net>
2015-12-04 18:37:25 -0500

    Add greensock and scroll magic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+$(function() {
+  var $frontCover = $('#front-cover');
+  var $frontCoverContent = $('#front-cover .cover-content');
 
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 'onLeave'
+    }
+  });
+
+  new ScrollMagic.Scene({
+        triggerElement: $frontCover,
+        duration: '50%'
+      })
+      .setPin($frontCover)
+      .addTo(controller);
+
+  TweenMax.set($frontCoverContent, {'-webkit-filter':'blur(0px)'});
+
+  var frontCoverBlur = TweenMax.to($frontCoverContent, 2, {
+    '-webkit-filter': 'blur(100px)',
+    //'filter': 'blur(100px)',
+    ease: Power1.easeInOut,
+    delay: 1
+  });
+});
